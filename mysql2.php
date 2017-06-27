9610cf5d3ff877fbdc7291c508728f2d1196f698
Brian.Wong <brian.wong@alphis.net>
2017-06-27 15:48:57 +0800

    $_POST & $_GET login form
    Made the $_POST & $_GET login form work, whilst linking them to MySQL.

--- mysql2.php
+++ mysql2.php
@@ -11,15 +11,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * from user WHERE username = '".$_POST['username']."' AND password ='".$_POST['password']."'";
+$sql = "SELECT * from user WHERE username = '".$_GET['username']."'";
+
 $result = mysqli_query($conn, $sql);
+// If username exists and password is correct, show Success Login
+// If username exists and password is wrong, show Wrong Password
+// If username does not exist, show Wrong Username
 
 //If there is more than 1 row, it means successful login
 if (mysqli_num_rows($result) > 0) {
-	echo "Successful login";
-} else {
+	//Username is 100% correct
+	
+	while($row = mysqli_fetch_assoc($result)) {
+		if ($row['password'] == $_GET['password']) {
+			echo "Succesful login<br>";
+			echo "<br>Your email is brian.wong@alphis.net";
+			
+		}
+		if ($row['password'] != $_GET['password']) 
+			
+			echo "Wrong password";
+		
+	}
+
+}
+else {
 	//0 rows, means unsuccessful login
-	echo "Failed login";
+	echo "Wrong Username";
 }
 mysqli_close($conn);
 
