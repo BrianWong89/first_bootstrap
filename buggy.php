041a8f819651f588c1977715362fbf7f66e5e42e
Brian.Wong <brian.wong@alphis.net>
2017-07-04 12:20:56 +0800

    Buggy script
    Fixed buggy script.
    
    -concatenation
    -use of '

---
+++ buggy.php
@@ -0,0 +1,11 @@
+<?php
+$greetings=array();
+$greetings["Merry Christmas"]="Merry Christmas!";
+
+
+//Amend the codes below only
+
+echo "We wish you a " . $greetings["Merry Christmas"] . "<br>"; // added a (") & (.) to both sides for string concatenation
+echo "Click <a href='https://www.youtube.com/watch?v=3Wv-JIpAjks'>here</a> for a Merry Christmas song."; // replaced (") w/ (') in URL link
+
+?>
