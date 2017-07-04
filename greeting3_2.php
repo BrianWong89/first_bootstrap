a26ec36c1405eda537fbd80dff62f3f2fdb652cb
Brian.Wong <brian.wong@alphis.net>
2017-07-04 12:16:09 +0800

    Indentation changes to greeting_2
    Tidied up code w/ proper indentation.

--- greeting3_2.php
+++ greeting3_2.php
@@ -4,12 +4,16 @@ $i = 0;
 $number = $_POST['number'];
 
 echo "<table border='1'>";
-while ($i < $number) {
+while (true) { //This code here also, must indent properly next time
 	echo "<tr>";
 	echo "<td>".$_POST['greeting']."</td>";
-	$i++;
 	echo "</tr>";
+	$i++;
+
+	if ($i == $number)
+		break;	
 }
+
 echo "</table>";
 
 ?>
