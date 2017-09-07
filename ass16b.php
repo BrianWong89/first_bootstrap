ea73a5dd8ec9a8c1607e9fb19d5def39297908c0
Brian.Wong <brian.wong@alphis.net>
2017-09-08 00:50:03 +0800

    ass16b (incomplete)
    will resume later in the morning

--- ass16b.php
+++ ass16b.php
@@ -15,6 +15,21 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 $response_array=json_decode($response,true);
-print_r($response_array);
+//print_r($response_array);
+//view-source:http://www.alphis.net/spellcasting-death.php?action=listActions
+
+$healthPoints=100;
+$manaPoints=100;
+
+echo "<table border='1'>\r\n";
+echo "<tr>\r\n<th>Spell</th><th>Current HP</th><th>Current MP</th>\r\n</tr>\r\n";
+
+foreach ($response_array as $member){
+	
+}
+
+//if () {
+	//echo "You have died instantly.";
+//}
 
 ?>
