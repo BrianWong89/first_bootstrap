fe017d4bc84461eedad468ae91a31017a555183c
BrianWong89 <brian.wong@alphis.net>
2017-09-11 14:04:24 +0800

    sql3 ver.2
    ver.2 of sql3

--- mysql_project/sql3.php
+++ mysql_project/sql3.php
@@ -12,6 +12,9 @@ foreach ($results as $row) {
 
     }
     DB::query("UPDATE `members` SET age = " . $dateofbirth->diff(new DateTime('now'))->y . " WHERE id = " . $row['id']);
+    //1) Do it with Placeholders means %s, %i
+    //2) Do it with MeekroDB's way.
+    //3) Use MeekroDB to add 2 more members, your brother and your father.
     $rows++;
 }
 
