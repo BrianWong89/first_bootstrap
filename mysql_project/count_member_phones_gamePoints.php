9e952164e018a4c3a0cdd71708375361f68b2da5
Brian.Wong <brian.wong@alphis.net>
2017-09-15 01:23:32 +0800

    ass16c
    code not finalized; requires a bit of touch-up.

--- mysql_project/count_member_phones_gamePoints.php
+++ mysql_project/count_member_phones_gamePoints.php
@@ -8,6 +8,34 @@
 require_once("vendor/autoload.php");
 
 //DB::debugMode();
-$results = DB::query("SELECT m.id, m.name, COUNT(*) as numberOfPhones FROM " .
-    "members m,members_phones mp WHERE m.id = mp.member_id " .
-    "GROUP by m.id");
+$results = DB::query("SELECT * FROM `members`");
+
+echo "<table border='1'>\r\n";
+echo "<tr>\r\n<th>Member ID</th><th>Name</th><th>Number of Phones</th><th>Number of Game Points</th>\r\n</tr>\r\n";
+
+$numberOfPhones = 0;
+$totalGamePoints = 0;
+foreach ($results as $member) {
+    $numberOfPhones = 0;
+    $phoneNumbers = DB::query("SELECT * FROM `members_phones` WHERE member_id = %i", $member['id']);
+    foreach ($phoneNumbers as $phoneNumber) {
+        $numberOfPhones++;
+    }
+    $gamePoints = DB::query("SELECT * FROM `members_game_points` WHERE member_id = %i", $member['id']);
+    $totalGamePoints = 0;
+    foreach ($gamePoints as $gamePoint) {
+        $totalGamePoints += $gamePoint['game_points'];
+    }
+    echo "<tr>\r\n";
+    echo "<td>" . $member['id'] . "</td>\r\n";
+    echo "<td>" . $member['name'] . "</td>\r\n";
+    echo "<td>" . $numberOfPhones . "</td>\r\n";
+    echo "<td>" . $totalGamePoints . "</td>\r\n";
+    echo "</tr>\r\n";
+}
+
+echo "</table>";
+
+?>
+
+
