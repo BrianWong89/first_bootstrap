2b6aa0e8380afea73c5fa7a2e4ba02252d3f8b6e
BrianWong89 <brian.wong@alphis.net>
2017-09-15 08:45:16 +0800

    ass16c (final version)
    completed ass16c.

--- ass16c.php
+++ ass16c.php
@@ -23,16 +23,30 @@ $response_array = json_decode($response, true);
 
 $healthPoints = 100;
 $manaPoints = 100;
+$invulnerability = false;
 
 echo "<table border='1'>\r\n";
 echo "<tr>\r\n<th>Spell</th><th>Current HP</th><th>Current MP</th>\r\n</tr>\r\n";
 
 foreach ($response_array as $spell) {
+    //print_r($spell);
+    //Check if damage spell AND invulnerable
+    if ($spell['type'] == "mana-shield") {
+        $invulnerability = true;
+    }
+    //This will trigger when the bubble shield blocks for you
+    if ($spell['type'] == "damage-spell" && $invulnerability == true) {
+        $invulnerability = false;
+        //Let's echo out the row to show that the damage spell was negated
+        echo "<tr>\r\n";
+        echo "<td>" . $spell['type'] . "</td>\r\n";
+        echo "<td>" . $healthPoints . "</td>\r\n";
+        echo "<td>" . $manaPoints . "</td>\r\n";
+        echo "</tr>\r\n";
+        continue;
+    }
     if ($spell['type'] == "damage-spell") {
         $healthPoints += $spell['hp'];
-    }
-    if ($spell['type'] == "mana-shield") {
-        $healthPoints += 80;
     }
     if ($spell['type'] == "heal-spell") {
         $healthPoints += $spell['hp'];
