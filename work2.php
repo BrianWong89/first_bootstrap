37fffeb5305a3d5e03a64ba034c47a3adc0bbcc2
BrianWong89 <brian.wong@alphis.net>
2017-09-15 10:54:05 +0800

    work 2 (create & push domains in a given variable)
    as per title.

---
+++ work2.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 15/9/2017
+ * Time: 9:38 AM
+ */
+$return = array();
+$return["domains"] = array();
+$domain1=array(
+    "domain" => "www.starhub.com.sg",
+    "country" => "Singapore",
+);
+$domain2=array(
+        "domain" => "www.cfirst.ae",
+        "country" => "United Arab Emirates");
+$domain3=array(
+        "domain" => "www.m1.com.sg",
+        "country" => "Singapore");
+
+array_push($return["domains"],$domain1);
+array_push($return["domains"],$domain2);
+array_push($return["domains"],$domain3);
+
+echo json_encode($return, JSON_PRETTY_PRINT);
