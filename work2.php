5bd5f061daf684f82cfd56d745579414a18e964b
Brian.Wong <brian.wong@alphis.net>
2017-09-19 00:23:35 +0800

    work 2
    minor formatting done

--- work2.php
+++ work2.php
@@ -7,19 +7,19 @@
  */
 $return = array();
 $return["domains"] = array();
-$domain1=array(
+$domain1 = array(
     "domain" => "www.starhub.com.sg",
     "country" => "Singapore",
 );
-$domain2=array(
-        "domain" => "www.cfirst.ae",
-        "country" => "United Arab Emirates");
-$domain3=array(
-        "domain" => "www.m1.com.sg",
-        "country" => "Singapore");
+$domain2 = array(
+    "domain" => "www.cfirst.ae",
+    "country" => "United Arab Emirates");
+$domain3 = array(
+    "domain" => "www.m1.com.sg",
+    "country" => "Singapore");
 
-array_push($return["domains"],$domain1);
-array_push($return["domains"],$domain2);
-array_push($return["domains"],$domain3);
+array_push($return["domains"], $domain1);
+array_push($return["domains"], $domain2);
+array_push($return["domains"], $domain3);
 
 echo json_encode($return, JSON_PRETTY_PRINT);
