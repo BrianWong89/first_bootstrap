790c3b621d89df5f829e62ddb859c7264ef57126
Brian.Wong <brian.wong@alphis.net>
2017-09-20 03:24:43 +0800

    basic bootstrap page
    used a bootstrap template from w3schools to create a simple webpage

---
+++ contact2.php
@@ -0,0 +1,16 @@
+<?php
+require("header2.php");
+?>
+    <div class="container-fluid text-center">
+        <div class="row content">
+            Contact me
+            <form>
+                <textarea></textarea>
+                <br>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+<?php
+require("footer2.php");
+?>
---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+require("header2.php");
+?>
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-8 text-left">
+            <p>My name is Brian. I'm 28 this year</p>
+        </div>
+    </div>
+</div>
+<?php
+require("footer2.php");
+?>
